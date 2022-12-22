89f7d6a3268b3742594f7943de26a40c675859aa
Mohit Sharma <mohitsharma90015656@gmail.com>
2022-12-22 19:37:35 +0530

    done

--- client/src/redux/action.js
+++ client/src/redux/action.js
@@ -1,5 +1,7 @@
 export const TOGGLE_AUTH = "TOGGLE_AUTH"
 
+
+
 export const toggleAuth = ()=>({
     type : TOGGLE_AUTH
 })
--- server/routes/Route.js
+++ server/routes/Route.js
@@ -2,11 +2,11 @@ import express from 'express';
 import { AddUser, getUser, getData, editData, deleatUser, signUpUser,  } from '../controller/UserController.js';
 const router = express.Router();
 
+
 router.post("/add", AddUser)
 router.get("/all", getUser)
 router.get("/:id", getData)
 router.post("/:id", editData)
 router.delete("/:id", deleatUser)
-
 
 export default router
